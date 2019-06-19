ec7297c5aa682ea616f4b4413120a90ae25912b7
Burak Ozter <burak.ozter@dal.ca>
2019-06-19 01:51:14 -0300

    Initial Commit

---
+++ Simple C++ Shell/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Simple C++ Shell
+//
+//  Created by Burak Can ÖZTER on 2019-06-19.
+//  Copyright © 2019 Burak Can ÖZTER. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
